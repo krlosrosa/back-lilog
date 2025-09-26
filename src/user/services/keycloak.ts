e78108df03d97c685dad2b1ef5d923d75c74535f
carlos <robertorsilva.krlos@gmail.com>
2025-09-26 13:39:25 -0300

    first commit

--- src/user/services/keycloak.ts
+++ src/user/services/keycloak.ts
@@ -16,7 +16,7 @@ export class KeycloakService implements IIdentityUserRepository {
       );
       this.kcAdminClient = new KcAdminClient({
         baseUrl: process.env.KEYCLOAK_URL,
-        realmName: 'lilo',
+        realmName: 'lilog',
       });
     }
     await this.kcAdminClient.auth({
